8ab6330b7c1a51d3d40a7ad1c7e2df4a5238d85c
Michael Schmitz <michael.schmitz@swissuccess.ch>
2025-05-31 12:09:45 +0200

    save entire neovim config

---
+++ neovim/nvim/lua/plugins/lsp-config.lua
@@ -0,0 +1,37 @@
+return {
+  {
+  "williamboman/mason.nvim",
+  config = function()
+    require("mason").setup()
+  end
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "lua_ls",
+          "ts_ls",
+          "bashls"
+        }
+      })
+    end
+  },
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = { "williamboman/mason-lspconfig.nvim" },
+    config = function()
+      local lspconfig = require("lspconfig")
+      lspconfig.lua_ls.setup({})
+      lspconfig.ts_ls.setup({})
+      lspconfig.bashls.setup({
+        filetypes = { "sh", "zsh" }
+      })
+
+      vim.keymap.set('n', '<leader>d', vim.lsp.buf.hover, {})
+      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+      vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, {})
+    end
+  }
+}
+
